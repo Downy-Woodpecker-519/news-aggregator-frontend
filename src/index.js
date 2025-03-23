7a14a5c3c0fbdfc769dbca043975457942d03071
Downy-Woodpecker-519 <ajberry807@outlook.com>
2025-03-23 07:18:37 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NewsApp from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NewsApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
